00b5473e91654b073dacff98b74cc7c82d1c67d4
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-08-25 13:56:21 +0300

    adding photos

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,15 @@
 import { FeatureBlock } from '../components/FeatureBlock'
 import { FeaturesGroup } from '../components/FeaturesGroup'
+import honor from '../assets/HonorX9A.jpg';
+import samsung from '../assets/SamsungA14.jpg'
+import apple from '../assets/iPhone14ProMax.jpg'
 export const LandingPage = () =>
 {
   return (
 	<>
-		<FeatureBlock />
-		<FeatureBlock />
-		<FeatureBlock />
+		<FeatureBlock title='Honor X9A' description='eXtra Vision, eXtra Power' link='Shop Now >' image={honor}/>
+		<FeatureBlock title='Galaxy A14' description='Colorfull colors' link='Shop Now >' image={samsung}/>
+		<FeatureBlock title=' iPhone 14 Pro Max' description='Stay Dynamic' link='Shop Now >' image={apple} invert/>
 		<div className='mx-3'>
 		<FeaturesGroup />
 		<FeaturesGroup />
