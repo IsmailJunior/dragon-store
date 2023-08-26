09c672c3717cb2acabd4af30a4489518dbd6ed09
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-08-26 14:30:02 +0300

    placholder

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,16 +1,15 @@
 import classes from './FeatureCard.module.css'
-import samsung from '../assets/S23UltraCard.jpg'
 
 export const FeatureCard = () =>
 {
   return (
 	<a href='#' className={ classes.FeatureCard }>
 		<div className='flex flex-col gap-2 z-10 absolute'>
-		<p>	SAMSUNG S23 ULTRA</p>
-		<h1 className='text-2xl'>New Start.</h1>
-		<p>From 999$.</p>
+		<p>TITLE</p>
+		<h1 className='text-2xl'>Heading.</h1>
+		<p>Description.</p>
 		</div>	
-		<figure className={classes.FeatureCardFigure} style={{backgroundImage: `url(${samsung})`}}></figure>
+		<figure className={classes.FeatureCardFigure} style={{backgroundImage: `url('https://i0.wp.com/roadmap-tech.com/wp-content/uploads/2019/04/placeholder-image.jpg?resize=400%2C400&ssl=1')`}}></figure>
 	</a>
   )
 }
