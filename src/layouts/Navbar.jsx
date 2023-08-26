0c8863eefd0595b5af42912d753f21981587b496
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-08-26 12:48:52 +0300

    landing page photos

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,10 +1,13 @@
+import dragon from '../assets/dragon.png'
 
 export const Navbar = () => {
 	return (
 	<nav className="p-4 z-20 text-sm bg-slate-100/60 backdrop-blur-lg fixed w-full">
 	<ul className="flex justify-between container">
 			<li>
-				<a href="#">Brand</a>
+				<a href="#">
+					<img width={30} src={dragon} alt="logo" />	
+				</a>
 			</li>
 			<li>
 				<a href="#">Store</a>
