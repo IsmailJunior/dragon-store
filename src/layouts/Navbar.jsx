3beaf20e974c17506a50dd8ac89a2267d05806d3
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-08-26 12:50:17 +0300

    logo

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,11 +1,14 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import dragon from '../assets/dragon.png'
 
 export const Navbar = () => {
 	return (
 	<nav className="p-4 z-20 text-sm bg-slate-100/60 backdrop-blur-lg fixed w-full">
 	<ul className="flex justify-between container">
 			<li>
-				<Link to='/'>Dragon Group</Link>
+				<Link to='/'>
+					<img width={30} src={dragon} alt="logo" />	
+				</Link>
 			</li>
 			<li>
 				<Link to='/store'>Store</Link>
