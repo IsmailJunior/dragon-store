5da90b2ff118c3dc8d6d14c3563334cce23d1da4
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-08-26 16:41:55 +0300

    cards

--- src/pages/StorePage.jsx
+++ src/pages/StorePage.jsx
@@ -1,7 +1,32 @@
 import { CategoryGroup } from '../components/CategoryGroup'
-import {FeatureCardGroup} from '../components/FeatureCardGroup'
+import { FeatureCardGroup } from '../components/FeatureCardGroup'
+import samsung from '../assets/S23Ultra.jpg'
+import apple from '../assets/iPhone14ProMaxCard.jpg'
 export const StorePage = () =>
 {
+
+	const data = {
+		firstSection: {
+		one: {
+			title: 'S23 ULTRA',
+			heading: '200MP Camera.',
+			description: 'From $999.',
+			image: samsung
+		},
+		two: {
+			title: 'IPHONE 14 PRO',
+			heading: 'Pro. Beyond.',
+			description: 'From $999.',
+			image: apple
+			},
+		three: {
+			title: 'IPHONE 14 PRO',
+			heading: 'Pro. Beyond.',
+			description: 'From $999.',
+			image: apple
+			},
+		}
+	}
 
   return (
 	<div className='container py-10'>
@@ -9,7 +34,7 @@ export const StorePage = () =>
 			<CategoryGroup />
 	</section>
 		<section>
-			<FeatureCardGroup />
+			<FeatureCardGroup headline='The latest. Take a look at what’s new, right now.' data={{data: data.firstSection}}/>
 			<FeatureCardGroup />
 			<FeatureCardGroup />
 			<FeatureCardGroup />
