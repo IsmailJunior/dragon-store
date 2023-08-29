c646aed07b55724c5670eb6758cb2a8b89cd360d
Merced <77894409+ismailMerced@users.noreply.github.com>
2023-08-29 14:39:07 +0300

    creating product components

--- src/components/RadioSqureContained.jsx
+++ src/components/RadioSqureContained.jsx
@@ -4,15 +4,11 @@ export const RadioSquareContained = ({id,name}) => {
 	<div>
 		<input className={classes.RadioSquare} type="radio" name={name} id={id} />
 		<label className="cursor-pointer" htmlFor={id}>
-			<span className="w-full border-2 border-neutral-400 rounded-xl flex flex-row justify-between p-3">
-			<span className="flex flex-col">
-				<span>Ttile</span>
-				<span>Price</span>	
+			<span className="border-2 border-neutral-400 rounded-xl flex flex-row justify-between p-3 px-40">
+			<span className="flex flex-col text-center">
+					<span>Cash</span>
+					<span className=' text-slate-600'>Description</span>
 			</span>
-			<span className="flex flex-col">
-				<span>Title</span>
-				<span>Price</span>
-			</span>	
 			</span>	
 		</label>
 	</div>
